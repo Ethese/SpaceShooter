3f2bdd40baad5e9eeda482d1d2887914f7fc11e8
Ethese <f.goyeneche.camus@gmail.com>
2020-04-09 12:22:14 -0400

    Avances Juego hasta sesión 07/04

---
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //publica o privada
+    //int float bool string
+    //nombre variable
+    //valor
+    [SerializeField]
+    private float speed;
+
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movimiento();
+    }
+
+    void Movimiento()
+    {
+        //new Vector3(1,0,0)
+        //new Vector3(0,1,0)
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+        transform.Translate(Vector3.up *Input.GetAxis("Vertical") * speed * Time.deltaTime);
+    }
+}
