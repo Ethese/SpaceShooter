377b872ba7c866608f23f1fcdb7d0b455f92e16d
Ethese <f.goyeneche.camus@gmail.com>
2020-04-30 15:22:42 -0400

    Added sprites(Player, Enemy and others)

--- Space Shooter/Assets/Scripts/Player.cs
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -65,7 +65,7 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
         {
             _canFire = Time.time + _fireRate;
-            Instantiate(_laser, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+            Instantiate(_laser, transform.position + new Vector3(0, 1.1f, 0), Quaternion.identity);
         }
     }
 
